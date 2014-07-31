834502d81bec772c2a2ed19def2f52a7c85a60ce
David Crawshaw <david.crawshaw@zentus.com>
2014-07-31 15:20:01 -0400

    go.mobile/example/hello: trivial language binding example
    LGTM=adonovan
    R=adonovan, adg
    CC=golang-codereviews
    https://golang.org/cl/116260043

---
+++ example/hello/main.go
@@ -0,0 +1,15 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"code.google.com/p/go.mobile/app"
+
+	_ "code.google.com/p/go.mobile/example/hello/hi/go_hi"
+)
+
+func main() {
+	app.Run()
+}
