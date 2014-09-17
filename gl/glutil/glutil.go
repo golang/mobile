b1ea4eba372761d3779324549f894f94a22a3d52
David Crawshaw <david.crawshaw@zentus.com>
2014-09-17 18:07:50 -0400

    go.mobile/gl: f32 utilities and extra debugging
    Split constants out into their own file. Hide the
    values in named types in a struct, so that when
    using the gldebug tag we can attach extra
    information (such as the name of a uniform and
    attribute).
    
    LGTM=nigeltao
    R=golang-codereviews, bryanturley, nigeltao
    CC=davidday, golang-codereviews
    https://golang.org/cl/137630043

--- gl/glutil/glutil.go
+++ gl/glutil/glutil.go
@@ -14,18 +14,18 @@ import (
 // CreateProgram creates, compiles, and links a gl.Program.
 func CreateProgram(vertexSrc, fragmentSrc string) (gl.Program, error) {
 	program := gl.CreateProgram()
-	if program == 0 {
-		return 0, fmt.Errorf("glutil: no programs available")
+	if program.Value == 0 {
+		return gl.Program{}, fmt.Errorf("glutil: no programs available")
 	}
 
 	vertexShader, err := loadShader(gl.VERTEX_SHADER, vertexSrc)
 	if err != nil {
-		return 0, err
+		return gl.Program{}, err
 	}
 	fragmentShader, err := loadShader(gl.FRAGMENT_SHADER, fragmentSrc)
 	if err != nil {
 		gl.DeleteShader(vertexShader)
-		return 0, err
+		return gl.Program{}, err
 	}
 
 	gl.AttachShader(program, vertexShader)
@@ -38,21 +38,21 @@ func CreateProgram(vertexSrc, fragmentSrc string) (gl.Program, error) {
 
 	if gl.GetProgrami(program, gl.LINK_STATUS) == 0 {
 		defer gl.DeleteProgram(program)
-		return 0, fmt.Errorf("glutil: %s", gl.GetProgramInfoLog(program))
+		return gl.Program{}, fmt.Errorf("glutil: %s", gl.GetProgramInfoLog(program))
 	}
 	return program, nil
 }
 
 func loadShader(shaderType gl.Enum, src string) (gl.Shader, error) {
 	shader := gl.CreateShader(shaderType)
-	if shader == 0 {
-		return 0, fmt.Errorf("glutil: could not create shader (type %v)", shaderType)
+	if shader.Value == 0 {
+		return gl.Shader{}, fmt.Errorf("glutil: could not create shader (type %v)", shaderType)
 	}
 	gl.ShaderSource(shader, src)
 	gl.CompileShader(shader)
 	if gl.GetShaderi(shader, gl.COMPILE_STATUS) == 0 {
 		defer gl.DeleteShader(shader)
-		return 0, fmt.Errorf("shader compile: %s", gl.GetShaderInfoLog(shader))
+		return gl.Shader{}, fmt.Errorf("shader compile: %s", gl.GetShaderInfoLog(shader))
 	}
 	return shader, nil
 }
