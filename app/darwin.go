5217175bab5d0d563c4d3f89df1108fed1760173
David Crawshaw <david.crawshaw@zentus.com>
2014-09-29 13:22:23 +1000

    go.mobile/app: darwin window drawing support for debugging
    This lets you run really simple apps in a window on an OS X
    machine. It's not an official target, but slightly more
    productive than waiting for an apk install when working on
    the sprite package.
    
    LGTM=adg
    R=golang-codereviews, adg
    CC=golang-codereviews
    https://golang.org/cl/147910043

---
+++ app/darwin.go
@@ -0,0 +1,110 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Simple on-screen app debugging for OS X. Not an officially supported
+// development target for apps, as screens with mice are very different
+// than screens with touch panels.
+
+// +build darwin
+
+package app
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Cocoa -framework OpenGL -framework QuartzCore
+#import <Cocoa/Cocoa.h>
+#import <OpenGL/gl.h>
+
+void glGenVertexArrays(GLsizei n, GLuint* array);
+void glBindVertexArray(GLuint array);
+
+void runApp(void);
+void lockContext(GLintptr);
+void unlockContext(GLintptr);
+double backingScaleFactor();
+*/
+import "C"
+import (
+	"log"
+	"runtime"
+	"sync"
+
+	"code.google.com/p/go.mobile/event"
+	"code.google.com/p/go.mobile/geom"
+	"code.google.com/p/go.mobile/gl"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func run(callbacks Callbacks) {
+	cb = callbacks
+	C.runApp()
+}
+
+//export setGeom
+func setGeom(scale, width, height float64) {
+	// Macs default to 72 DPI, so scales are equivalent.
+	geom.Scale = float32(scale)
+	geom.Width = geom.Pt(width)
+	geom.Height = geom.Pt(height)
+	log.Printf("geom: Scale=%.2f, Width=%v, Height=%v", geom.Scale, geom.Width, geom.Height)
+}
+
+func initGL() {
+	// Using attribute arrays in OpenGL 3.3 requires the use of a VBA.
+	// But VBAs don't exist in ES 2. So we bind one default one.
+	var id C.GLuint
+	C.glGenVertexArrays(1, &id)
+	C.glBindVertexArray(id)
+}
+
+var cb Callbacks
+var initGLOnce sync.Once
+var events = make(chan event.Touch, 1<<6)
+
+func sendTouch(ty event.TouchType, x, y float32) {
+	events <- event.Touch{
+		Type: ty,
+		Loc: geom.Point{
+			X: geom.Pt(x),
+			Y: geom.Height - geom.Pt(y),
+		},
+	}
+}
+
+//export eventMouseDown
+func eventMouseDown(x, y float32) { sendTouch(event.TouchStart, x, y) }
+
+//export eventMouseMove
+func eventMouseMove(x, y float32) { sendTouch(event.TouchMove, x, y) }
+
+//export eventMouseEnd
+func eventMouseEnd(x, y float32) { sendTouch(event.TouchEnd, x, y) }
+
+//export drawgl
+func drawgl(ctx C.GLintptr) {
+	runtime.LockOSThread()
+	C.lockContext(ctx)
+
+	initGLOnce.Do(initGL)
+
+loop:
+	for {
+		select {
+		case e := <-events:
+			cb.Touch(e)
+		default:
+			break loop
+		}
+	}
+
+	gl.ClearColor(0, 0, 0, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	cb.Draw()
+
+	C.unlockContext(ctx)
+	runtime.UnlockOSThread()
+}
