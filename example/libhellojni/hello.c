969a1bf7ddd4ab3d80c165099787f5bf92ca356a
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2014-12-09 17:08:14 -0500

    example/libhellojni: fix build and allow go get golang.org/x/mobile/...
    Change-Id: Id0cd58705c882ea58cedbee5a6e9aff010c0ad4d
    Reviewed-on: https://go-review.googlesource.com/1281
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- example/libhellojni/hello.c
+++ example/libhellojni/hello.c
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build android
+
 // See main.go for commentary.
 
 #include <android/log.h>
