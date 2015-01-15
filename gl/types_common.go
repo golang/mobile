a8fcf739b918080eb1fa3ac8bc59e9927b31a097
Hyang-Ah Hana Kim <hakim@google.com>
2015-01-15 14:32:55 -0500

    mobile: limit app/font/gl packages to linux/darwin.
    They don't make sense for other platforms yet.
    
    See golang/go#9603
    
    Change-Id: I2ed269a00b1e1961aadf0a47fc95bbe3cd230113
    Reviewed-on: https://go-review.googlesource.com/2902
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- gl/types_common.go
+++ gl/types_common.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux darwin
+
 package gl
 
 // This file contains GL Types and their methods that are independent of the
