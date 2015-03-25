74be7c24e457516032cb3915076600ae908f4eed
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-03-25 19:09:35 -0400

    cmd/gomobile: update the doc.
    Change-Id: Ie654ad9aaaf5a1c0394731161194a1358a075e97
    Reviewed-on: https://go-review.googlesource.com/8245
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- cmd/gomobile/bind.go
+++ cmd/gomobile/bind.go
@@ -41,18 +41,20 @@ Bind generates language bindings like gobind (golang.org/x/mobile/cmd/gobind)
 for a package and builds a shared library for each platform from the go binding
 code.
 
-For Android, the bind command produces an AAR file that archives the precompiled
-Java API stub classes, the compiled shared libraries, and all asset files in
-the /assets subdirectory under the package directory. The output AAR file name
-is '<package_name>.aar'.
+For Android, the bind command produces an AAR (Android ARchive) file that
+archives the precompiled Java API stub classes, the compiled shared libraries,
+and all asset files in the /assets subdirectory under the package directory.
+The output AAR file name is '<package_name>.aar'.
+
+The AAR file is commonly used for binary distribution of an Android library
+project and most Android IDEs support AAR import. For example, in Android
+Studio (1.2+), an AAR file can be imported using the module import wizard
+(File > New > New Module > Import .JAR or .AAR package), and setting it as
+a new dependency (File > Project Structure > Dependencies).
 
 This command requires 'javac' (version 1.7+) and Android SDK (API level 9
 or newer) to build the library for Android. The environment variable
 ANDROID_HOME must be set to the path to Android SDK.
-
-For more details of AAR file format, see:
-
-   http://tools.android.com/tech-docs/new-build-system/aar-format
 
 The -v flag provides verbose output, including the list of packages built.
 
