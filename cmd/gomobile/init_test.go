a2d67a2fa4f0aed305005d3a27eac8cb117047ab
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-04-20 17:11:20 -0400

    cmd/gomobile: fix init_test.go to pass on windows/darwin.
    Change-Id: I03a18267ddd35ce432a394732185dab0283f2da6
    Reviewed-on: https://go-review.googlesource.com/9170
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- cmd/gomobile/init_test.go
+++ cmd/gomobile/init_test.go
@@ -7,6 +7,8 @@ package main
 import (
 	"bytes"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 	"text/template"
 )
@@ -24,8 +26,12 @@ func TestInit(t *testing.T) {
 	buildN = true
 	buildX = true
 	// Test that first GOPATH element is chosen correctly.
-	os.Setenv("GOPATH", "GOPATH1:/path2:/path3")
-
+	paths := []string{"GOPATH1", "/path2", "/path3"}
+	os.Setenv("GOPATH", strings.Join(paths, string(os.PathListSeparator)))
+	os.Setenv("GOROOT_BOOTSTRAP", "go1.4")
+	if goos == "windows" {
+		os.Setenv("HOMEDRIVE", "C:")
+	}
 	err := runInit(cmdInit)
 	if err != nil {
 		t.Log(buf.String())
@@ -42,8 +48,20 @@ func TestInit(t *testing.T) {
 }
 
 func diffOutput(got string, wantTmpl *template.Template) (string, error) {
+	got = filepath.ToSlash(got)
+
 	wantBuf := new(bytes.Buffer)
-	data := outputData{ndkVersion, goos, goarch, ndkarch}
+	data := outputData{
+		NDK:         ndkVersion,
+		GOOS:        goos,
+		GOARCH:      goarch,
+		NDKARCH:     ndkarch,
+		BuildScript: unixBuildScript,
+	}
+	if goos == "windows" {
+		data.EXE = ".exe"
+		data.BuildScript = windowsBuildScript
+	}
 	if err := wantTmpl.Execute(wantBuf, data); err != nil {
 		return "", err
 	}
@@ -55,11 +73,18 @@ func diffOutput(got string, wantTmpl *template.Template) (string, error) {
 }
 
 type outputData struct {
-	NDK     string
-	GOOS    string
-	GOARCH  string
-	NDKARCH string
+	NDK         string
+	GOOS        string
+	GOARCH      string
+	NDKARCH     string
+	EXE         string // .extension for executables. (ex. ".exe" for windows)
+	BuildScript string
 }
+
+const (
+	unixBuildScript    = `PATH=$PATH GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 CC_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-gcc CXX_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-g++ TMPDIR=$WORK HOME=$HOME GOROOT_BOOTSTRAP=go1.4 $WORK/go/src/make.bash --no-clean`
+	windowsBuildScript = `PATH=$PATH GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 CC_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-gcc.exe CXX_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-g++.exe TEMP=$WORK TMP=$WORK HOMEDRIVE=C: HOMEPATH=$HOMEPATH GOROOT_BOOTSTRAP=go1.4 $WORK/go/src/make.bat --no-clean`
+)
 
 var initTmpl = template.Must(template.New("output").Parse(`NDKCCPATH=GOPATH1/pkg/gomobile/android-{{.NDK}}
 rm -r -f "$NDKCCPATH"
@@ -78,25 +103,25 @@ mv $WORK/android-{{.NDK}}/toolchains/arm-linux-androideabi-4.8/prebuilt/{{.GOOS}
 mv $WORK/android-{{.NDK}}/toolchains/arm-linux-androideabi-4.8/prebuilt/{{.GOOS}}-{{.NDKARCH}}/lib $NDKCCPATH/arm/lib
 mv $WORK/android-{{.NDK}}/toolchains/arm-linux-androideabi-4.8/prebuilt/{{.GOOS}}-{{.NDKARCH}}/libexec $NDKCCPATH/arm/libexec
 mkdir -p $NDKCCPATH/arm/arm-linux-androideabi/bin
-ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-ld $NDKCCPATH/arm/arm-linux-androideabi/bin/ld
-ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-as $NDKCCPATH/arm/arm-linux-androideabi/bin/as
-ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-gcc $NDKCCPATH/arm/arm-linux-androideabi/bin/gcc
-ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-g++ $NDKCCPATH/arm/arm-linux-androideabi/bin/g++
+ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-ld{{.EXE}} $NDKCCPATH/arm/arm-linux-androideabi/bin/ld{{.EXE}}
+ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-as{{.EXE}} $NDKCCPATH/arm/arm-linux-androideabi/bin/as{{.EXE}}
+ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-gcc{{.EXE}} $NDKCCPATH/arm/arm-linux-androideabi/bin/gcc{{.EXE}}
+ln -s $NDKCCPATH/arm/bin/arm-linux-androideabi-g++{{.EXE}} $NDKCCPATH/arm/arm-linux-androideabi/bin/g++{{.EXE}}
 curl -o$WORK/gomobile-openal-soft-1.16.0.1.tar.gz https://dl.google.com/go/mobile/gomobile-openal-soft-1.16.0.1.tar.gz
 tar xfz gomobile-openal-soft-1.16.0.1.tar.gz
 mv $WORK/openal/include/AL $NDKCCPATH/arm/sysroot/usr/include/AL
 mkdir -p $NDKCCPATH/openal
 mv $WORK/openal/lib $NDKCCPATH/openal/lib
-PATH=$PATH GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 CC_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-gcc CXX_FOR_TARGET=$NDKCCPATH/arm/bin/arm-linux-androideabi-g++ TMPDIR=$WORK HOME=$HOME $WORK/go/src/make.bash --no-clean
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/5l $NDKCCPATH/arm/bin/5l
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/5g $NDKCCPATH/arm/bin/5g
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/asm $NDKCCPATH/arm/bin/asm
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/cgo $NDKCCPATH/arm/bin/cgo
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/nm $NDKCCPATH/arm/bin/nm
-mv $WORK/go/pkg/tool/linux_amd64/old5a $NDKCCPATH/arm/bin/old5a
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/pack $NDKCCPATH/arm/bin/pack
-mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/link $NDKCCPATH/arm/bin/link
-go build -o $NDKCCPATH/arm/bin/toolexec $WORK/toolexec.go
+{{.BuildScript}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/5l{{.EXE}} $NDKCCPATH/arm/bin/5l{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/5g{{.EXE}} $NDKCCPATH/arm/bin/5g{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/asm{{.EXE}} $NDKCCPATH/arm/bin/asm{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/cgo{{.EXE}} $NDKCCPATH/arm/bin/cgo{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/nm{{.EXE}} $NDKCCPATH/arm/bin/nm{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/old5a{{.EXE}} $NDKCCPATH/arm/bin/old5a{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/pack{{.EXE}} $NDKCCPATH/arm/bin/pack{{.EXE}}
+mv $WORK/go/pkg/tool/{{.GOOS}}_{{.GOARCH}}/link{{.EXE}} $NDKCCPATH/arm/bin/link{{.EXE}}
+go build -o $NDKCCPATH/arm/bin/toolexec{{.EXE}} $WORK/toolexec.go
 rm -r -f "$GOROOT/pkg/android_arm"
 mv $WORK/go/pkg/android_arm $GOROOT/pkg/android_arm
 go version > GOPATH1/pkg/gomobile/version
