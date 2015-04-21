717c2c43126e4d1b55ee187d52f72272276065e4
Burcu Dogan <jbd@google.com>
2015-04-21 14:51:13 -0700

    example/basic: add build instructions for gomobile
    This CL also removes Android build system required files and keeping
    it to the bare minimum to what gomobile requires.
    
    Fixes #10523.
    
    Change-Id: Iabf16e7ea040a74eaf64746e7cc4e23ef3683af2
    Reviewed-on: https://go-review.googlesource.com/9204
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- example/basic/main.go
+++ example/basic/main.go
@@ -2,7 +2,65 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// TODO(jbd): Refer to http://golang.org/doc/install/source
+// once Go 1.5 instructions are complete.
+
 // An app that draws a green triangle on a red background.
+//
+// Note: This demo is an early preview of Go 1.5. In order to build this
+// program as an Android APK using the gomobile tool, you need to install
+// Go 1.5 from the source.
+//
+// Clone the source from the tip under $HOME/go directory. On Windows,
+// you may like to clone the repo to your user folder, %USERPROFILE%\go.
+//
+//   $ git clone https://go.googlesource.com/go $HOME/go
+//
+// Go 1.5 requires Go 1.4. Read more about this requirement at
+// http://golang.org/s/go15bootstrap.
+// Set GOROOT_BOOTSTRAP to the GOROOT of your existing 1.4 installation or
+// follow the steps below to checkout go1.4 from the source and build.
+//
+//   $ git clone https://go.googlesource.com/go $HOME/go1.4
+//   $ cd $HOME/go1.4
+//   $ git checkout go1.4.1
+//   $ cd src && ./make.bash
+//
+// If you clone Go 1.4 to a different destination, set GOROOT_BOOTSTRAP
+// environmental variable accordingly.
+//
+// Build Go 1.5 and add Go 1.5 bin to your path.
+//
+//   $ cd $HOME/go/src && ./make.bash
+//   $ export PATH=$PATH:$HOME/go/bin
+//
+// Set a GOPATH if no GOPATH is set, add $GOPATH/bin to your path.
+//
+//   $ export GOPATH=$HOME
+//   $ export PATH=$PATH:$GOPATH/bin
+//
+// Get the gomobile tool and initialize.
+//
+//   $ go get golang.org/x/mobile/cmd/gomobile
+//   $ gomobile init
+//
+// It may take a while to initialize gomobile, please wait.
+//
+// Get the basic example and use gomobile to build or install it on your device.
+//
+//   $ go get -d golang.org/x/mobile/example/basic
+//   $ gomobile build golang.org/x/mobile/example/basic # will build an APK
+//
+//   # plug your Android device to your computer or start an Android emulator.
+//   # if you have adb installed on your machine, use gomobile install to
+//   # build and deploy the APK to an Android target.
+//   $ gomobile install golang.org/x/mobile/example/basic
+//
+// Switch to your device or emulator to start the Basic application from
+// the launcher.
+// You can also run the application on your desktop by running the command
+// below. (Note: It currently doesn't work on Windows.)
+//   $ go install golang.org/x/mobile/example/basic && basic
 package main
 
 import (
