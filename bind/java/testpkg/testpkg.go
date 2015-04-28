9bdb5650628e4a16f8d3f2d09fd14b9602f05e22
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-04-28 12:29:48 -0400

    bind/java: add mising copyright statements.
    Change-Id: Ifb120cba013d48fa83af41a43ccbbe7de87b26c8
    Reviewed-on: https://go-review.googlesource.com/9408
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- bind/java/testpkg/testpkg.go
+++ bind/java/testpkg/testpkg.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package testpkg contains bound functions for testing the cgo-JNI interface.
 // This is used in tests of golang.org/x/mobile/bind/java.
 package testpkg
