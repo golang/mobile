7358f397e06be6b90be8a3672b20f8b2ba59aad1
Burcu Dogan <jbd@google.com>
2015-05-04 20:30:44 -0400

    mobile: add missing canonical import paths
    Change-Id: I71d38a6494b8235f5669ff53e6bd29e7045d0409
    Reviewed-on: https://go-review.googlesource.com/9697
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package sensor provides sensor events from various movement sensors.
-package sensor
+package sensor // import "golang.org/x/mobile/sensor"
 
 import (
 	"errors"
