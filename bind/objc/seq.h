5c3e18f08f98eedaa1a0bbfb44f24ccd42ae59b8
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-05-05 15:57:36 -0400

    bind/objc: obj-c binding for functions using numeric, string types.
    test.bash is a simple script that builds the c-shared library from
    test_main.go, compiles SeqTest.m and the objective-c bindings of testpkg
    (testpkg/objc_testpkg/GoTestpkg.*), and runs the output. Eventually this
    will be replaced with coed that runs gomobile bind & xcodebuild.
    
    The code under testpkg/go_testpkg and testpkg/objc_testpkg is the output
    of gobind (now manually-generated). I am adding it to repo now in order
    to get the testpkg/objc_tstpkg reviewed. Eventually, this will be
    removed from the repo.
    
    Change-Id: I8d6af3732337992af922cb4615a63f385e19d489
    Reviewed-on: https://go-review.googlesource.com/9826
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- bind/objc/seq.h
+++ bind/objc/seq.h
@@ -5,8 +5,36 @@
 #ifndef __GO_SEQ_HDR__
 #define __GO_SEQ_HDR__
 
-// GoSeq is a sequnce of machine-dependent encoded values.
+#include <Foundation/Foundation.h>
+
+// GoSeq is a sequence of machine-dependent encoded values.
 // Used by automatically generated language bindings to talk to Go.
 typedef struct GoSeq { void *mem_ptr; } GoSeq;
+
+// go_seq_free frees resources of the GoSeq.
+// It does not free the GoSeq itself.
+extern void go_seq_free(GoSeq *seq);
+
+extern int8_t go_seq_readInt8(GoSeq *seq);
+extern int16_t go_seq_readInt16(GoSeq *seq);
+extern int32_t go_seq_readInt32(GoSeq *seq);
+extern int64_t go_seq_readInt64(GoSeq *seq);
+extern float go_seq_readFloat32(GoSeq *seq);
+extern double go_seq_readFloat64(GoSeq *seq);
+extern NSString *go_seq_readUTF8(GoSeq *seq);
+
+extern void go_seq_writeInt8(GoSeq *seq, int8_t v);
+extern void go_seq_writeInt16(GoSeq *seq, int16_t v);
+extern void go_seq_writeInt32(GoSeq *seq, int32_t v);
+extern void go_seq_writeInt64(GoSeq *seq, int64_t v);
+extern void go_seq_writeFloat32(GoSeq *seq, float v);
+extern void go_seq_writeFloat64(GoSeq *seq, double v);
+extern void go_seq_writeUTF8(GoSeq *seq, NSString *v);
+
+// go_seq_send sends a function invocation request to Go.
+// It blocks until the function completes.
+// If the request is for a method, the first element in req is
+// a Ref to the receiver.
+extern void go_seq_send(char *descriptor, int code, GoSeq *req, GoSeq *res);
 
 #endif // __GO_SEQ_HDR__
