4d3dd47e872a5c7742d0fd1faa49db986900dd40
Nigel Tao <nigeltao@golang.org>
2015-06-21 20:02:37 +1000

    app/debug: use the *image.RGBA drawing fast path.
    On my 2013 Nexus 7, a simple
    
    start := time.Now()
    draw.Draw(etc)
    log.Printf("draw.Draw took %v", time.Since(start))
    
    around the draw.Draw call dropped from 12ms to 30µs.
    
    Change-Id: I12aea4a4ade07eedb8ee86f7c8d4a4ed49f39f36
    Reviewed-on: https://go-review.googlesource.com/11299
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- app/debug/fps.go
+++ app/debug/fps.go
@@ -57,7 +57,7 @@ func DrawFPS() {
 	now := time.Now()
 	diff := now.Sub(lastDraw)
 	str := fmt.Sprintf("%.0f FPS", float32(time.Second)/float32(diff))
-	draw.Draw(fps.Image, fps.Image.Rect, image.White, image.Point{}, draw.Src)
+	draw.Draw(fps.Image.RGBA, fps.Image.Rect, image.White, image.Point{}, draw.Src)
 
 	ftpt12 := freetype.Pt(0, int(12*geom.PixelsPerPt))
 	if _, err := monofont.DrawString(str, ftpt12); err != nil {
