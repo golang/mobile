8de8fcfbf73a80a0ace6dacfcdc384b86a77995b
Nigel Tao <nigeltao@golang.org>
2015-06-30 16:59:14 +1000

    app/debug, exp/font: remove the freetype dependency.
    Change-Id: Id71acfeb605995d8caf947d996a1375335410e08
    Reviewed-on: https://go-review.googlesource.com/11668
    Reviewed-by: David Crawshaw <crawshaw@golang.org>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- exp/font/font_test.go
+++ exp/font/font_test.go
@@ -7,16 +7,40 @@
 package font
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
-
-	"code.google.com/p/freetype-go/freetype"
 )
 
-func TestLoadFonts(t *testing.T) {
-	if _, err := freetype.ParseFont(Default()); err != nil {
-		t.Fatalf("default font: %v", err)
+func looksLikeATTF(b []byte) error {
+	if len(b) < 256 {
+		return fmt.Errorf("not a TTF: not enough data")
 	}
-	if _, err := freetype.ParseFont(Monospace()); err != nil {
-		t.Fatalf("monospace font: %v", err)
+	b = b[:256]
+
+	// Look for the 4-byte magic header. See
+	// http://www.microsoft.com/typography/otspec/otff.htm
+	switch string(b[:4]) {
+	case "\x00\x01\x00\x00", "ttcf":
+		// No-op.
+	default:
+		return fmt.Errorf("not a TTF: missing magic header")
+	}
+
+	// Look for a glyf table.
+	if i := bytes.Index(b, []byte("glyf")); i < 0 {
+		return fmt.Errorf(`not a TTF: missing "glyf" table`)
+	} else if i%0x10 != 0x0c {
+		return fmt.Errorf(`not a TTF: invalid "glyf" offset 0x%02x`, i)
+	}
+	return nil
+}
+
+func TestLoadFonts(t *testing.T) {
+	if err := looksLikeATTF(Default()); err != nil {
+		t.Errorf("default font: %v", err)
+	}
+	if err := looksLikeATTF(Monospace()); err != nil {
+		t.Errorf("monospace font: %v", err)
 	}
 }
