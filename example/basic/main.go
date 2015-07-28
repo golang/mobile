efdfc86159a812b2221e871b14195b4f71cc9fd0
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-07-28 12:29:19 -0400

    example: limit examples build to linux or darwin.
    windows and other non-mobile os support is not there yet.
    
    Change-Id: Ie9c456b646bfa0b0c489e1b6344b5afca4801c5f
    Reviewed-on: https://go-review.googlesource.com/12744
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- example/basic/main.go
+++ example/basic/main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin linux
+
 // An app that draws a green triangle on a red background.
 //
 // Note: This demo is an early preview of Go 1.5. In order to build this
