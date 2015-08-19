e959dea03da97fb327cf69e6e780b037196a7760
Nigel Tao <nigeltao@golang.org>
2015-08-19 15:29:12 +1000

    event/key: implement Code.String, via stringer.
    Change-Id: I4acf4f351a30ee64192ed9f896ca31d29fe0397f
    Reviewed-on: https://go-review.googlesource.com/13712
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- event/key/key.go
+++ event/key/key.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:generate stringer -type=Code
+
 // Package key defines an event for physical keyboard keys.
 //
 // On-screen software keyboards do not send key events.
