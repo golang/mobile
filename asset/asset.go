49a82fefe311be8d7dadba6ef38425bf31c663e4
Hana Kim <hyangah@gmail.com>
2015-08-21 17:36:09 -0400

    all: support go get golang.org/x/mobile/... on Windows
    Fixes golang/go#12212
    Workaround for golang/go#12261 until golang/go#9306 is fixed.
    
    Change-Id: I51c1bcfc92c1553fe2132586a0234b1c1af6aeb1
    Reviewed-on: https://go-review.googlesource.com/13745
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- asset/asset.go
+++ asset/asset.go
@@ -4,18 +4,6 @@
 
 // +build darwin linux
 
-// Package asset provides access to application-bundled assets.
-//
-// On Android, assets are accessed via android.content.res.AssetManager.
-// These files are stored in the assets/ directory of the app. Any raw asset
-// can be accessed by its direct relative name. For example assets/img.png
-// can be opened with Open("img.png").
-//
-// On iOS an asset is a resource stored in the application bundle.
-// Resources can be loaded using the same relative paths.
-//
-// For consistency when debugging on a desktop, assets are read from a
-// directory named assets under the current working directory.
 package asset
 
 import "io"
--- exp/sprite/glsprite/glsprite.go
+++ exp/sprite/glsprite/glsprite.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin linux
+
 // Package glsprite implements a sprite Engine using OpenGL ES 2.
 //
 // Each sprite.Texture is loaded as a GL texture object and drawn
