e766bbfb3ce9009548d1ccba600bff64a74f47ea
Hyang-Ah (Hana) Kim <hyangah@gmail.com>
2015-10-01 12:36:01 -0400

    cmd/gomobile: fix minSdkVersion formatting for bind.
    The fix for golang/go#12661 was incorrect.
    
    Fixes golang/go#12809.
    
    Change-Id: Ic2cfedcc99ced0241a5756fb36f6f9ae5a6b7287
    Reviewed-on: https://go-review.googlesource.com/15270
    Reviewed-by: Burcu Dogan <jbd@google.com>

--- cmd/gomobile/bind_androidapp.go
+++ cmd/gomobile/bind_androidapp.go
@@ -156,7 +156,7 @@ func buildAAR(androidDir string, pkg *build.Package) (err error) {
 		return err
 	}
 	const manifestFmt = `<manifest xmlns:android="http://schemas.android.com/apk/res/android" package=%q>
-<uses-sdk android:minSdkVersion=%q/></manifest>`
+<uses-sdk android:minSdkVersion="%d"/></manifest>`
 	fmt.Fprintf(w, manifestFmt, "go."+pkg.Name+".gojni", minAndroidAPI)
 
 	w, err = aarwcreate("proguard.txt")
