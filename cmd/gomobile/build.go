71a0e8bda1c3e2cedddd67299021cd6044070c91
Sridhar Venkatakrishnan <sridhar@laddoo.net>
2015-11-19 10:51:29 +0530

    bind: Android support for multiple packages.
    As discussed in golang/go#12245
    
    Usage: gomobile bind [options] a.b.c x.y.z
    
    For java gobind and gomobile  will generate go.c.C.java and go.z.Z.java.
    If -javapkg=com.example is specified they will generate
    com.example.C.java and com.example.Z.java.
    
    Tested on Darwin.
    
    Change-Id: Ia8e57c8fec7967131d55de71cc705d9e736ccca0
    Reviewed-on: https://go-review.googlesource.com/17023
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- cmd/gomobile/build.go
+++ cmd/gomobile/build.go
@@ -248,14 +248,14 @@ func init() {
 }
 
 func goBuild(src string, env []string, args ...string) error {
-	return goCmd("build", src, env, args...)
+	return goCmd("build", []string{src}, env, args...)
 }
 
-func goInstall(src string, env []string, args ...string) error {
-	return goCmd("install", src, env, args...)
+func goInstall(srcs []string, env []string, args ...string) error {
+	return goCmd("install", srcs, env, args...)
 }
 
-func goCmd(subcmd, src string, env []string, args ...string) error {
+func goCmd(subcmd string, srcs []string, env []string, args ...string) error {
 	// The -p flag is to speed up darwin/arm builds.
 	// Remove when golang.org/issue/10477 is resolved.
 	cmd := exec.Command(
@@ -284,7 +284,7 @@ func goCmd(subcmd, src string, env []string, args ...string) error {
 		cmd.Args = append(cmd.Args, "-work")
 	}
 	cmd.Args = append(cmd.Args, args...)
-	cmd.Args = append(cmd.Args, src)
+	cmd.Args = append(cmd.Args, srcs...)
 	cmd.Env = append([]string{}, env...)
 	return runCmd(cmd)
 }
