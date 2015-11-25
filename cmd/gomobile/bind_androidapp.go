2454c829c13fd885622b49120cfb6c7213889449
David Crawshaw <crawshaw@golang.org>
2015-11-25 09:25:02 -0500

    bind/java: move android-specific context code
    Change-Id: I7b1dfae97576c7bb2d45e6c2a5732e20df79a77b
    Reviewed-on: https://go-review.googlesource.com/17251
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- cmd/gomobile/bind_androidapp.go
+++ cmd/gomobile/bind_androidapp.go
@@ -101,9 +101,29 @@ func goAndroidBind(pkgs []*build.Package) error {
 
 var loadSrc = `package go;
 
+import android.app.Application;
+import android.content.Context;
+
+import java.util.logging.Logger;
+
 public class LoadJNI {
+	private static Logger log = Logger.getLogger("GoLoadJNI");
+
+	public static final Object ctx;
+
 	static {
 		System.loadLibrary("gojni");
+
+		Object androidCtx = null;
+		try {
+			// TODO(hyangah): check proguard rule.
+			Application appl = (Application)Class.forName("android.app.AppGlobals").getMethod("getInitialApplication").invoke(null);
+			androidCtx = appl.getApplicationContext();
+		} catch (Exception e) {
+                        log.warning("Global context not found: " + e);
+		} finally {
+			ctx = androidCtx;
+		}
 	}
 }
 `
