563f2bdd3a190aa2f2d0b37ae419cfa9ee02d229
Hyang-Ah Hana Kim <hyangah@gmail.com>
2015-12-11 18:47:37 -0500

    cmd/gomobile: update hashes of gomobile-ndk-*
    These tar.gz include x86, x86_64 tools and libraries.
    This CL will be submitted once the files are available from dl servers.
    
    Updates golang/go#10743
    
    Change-Id: I4dd59221f392d6daf4d13ab7af38d01b8e6f4546
    Reviewed-on: https://go-review.googlesource.com/17560
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- cmd/gomobile/hashes.go
+++ cmd/gomobile/hashes.go
@@ -4,16 +4,18 @@
 
 package main
 
+// Hashes were computed with 'go run release.go'
+
 var fetchHashes = map[string]string{
 	"android-ndk-r10e-darwin-x86_64.bin":      "728c309e606f63101f1258c9d3d579b80ac74fe74c511ebb71f460ce5c5d084e",
+	"gomobile-ndk-r10e-darwin-x86_64.tar.gz":  "9185a734b1b3d8dd29f3bc3c55cf03e397414e656345b7ed835879ee8eb76ebb",
 	"android-ndk-r10e-linux-x86.bin":          "92b07d25aaad9b341a7f2b2a62402d508e948bf2dea3ee7b65a6aeb18bca7df5",
+	"gomobile-ndk-r10e-linux-x86.tar.gz":      "eafb03ba5cf4712673f44a0991579c235774ff185dd846c1c5ecb3527acac22b",
 	"android-ndk-r10e-linux-x86_64.bin":       "102d6723f67ff1384330d12c45854315d6452d6510286f4e5891e00a5a8f1d5a",
+	"gomobile-ndk-r10e-linux-x86_64.tar.gz":   "709d96f5234c224e4b72254fd2e196d0e1486b8d943e972ed98cf14e3ca7bdde",
 	"android-ndk-r10e-windows-x86.exe":        "5378a60a796de75a2d609b76f5c56df7a343511ec36a823bae8c7423591cc8f5",
+	"gomobile-ndk-r10e-windows-x86.tar.gz":    "5a2da001af85d2b9baf257c556159dc3e0a92f115d2e3d290894dcd8dfe9552e",
 	"android-ndk-r10e-windows-x86_64.exe":     "9bc55ea4b3ad38dcd34157528760e62183b63ebc4a38a6ca478889ff7583d9c1",
-	"gomobile-ndk-r10e-darwin-x86_64.tar.gz":  "fa69cb2523e5c35e72c7dd24b2db594b67f79dc237db6b1becd5e24368e973e8",
-	"gomobile-ndk-r10e-linux-x86.tar.gz":      "e38e28503cd52c795bc6703d67c3e158f59d28e2e4f1e727d7a31c52a7fbee14",
-	"gomobile-ndk-r10e-linux-x86_64.tar.gz":   "7c18c015cbb86526a00e5f82261947889cf4bc379d6d583a66794cc45451106b",
-	"gomobile-ndk-r10e-windows-x86.tar.gz":    "bbf4752c3c52acf75b8c1f6e1e4c6c4b5e8350c8f4e01c01061b05bd917f633e",
-	"gomobile-ndk-r10e-windows-x86_64.tar.gz": "41c0fb8ad805088a6bed56a96d2bab9d4daeed77d38c3462b4af33378edf6a67",
-	"gomobile-openal-soft-1.16.0.1.tar.gz":    "fcdbf21bccacf41f65e94a049e0fe939391ecc8d3128ae0db7d1d062f32659ca",
+	"gomobile-ndk-r10e-windows-x86_64.tar.gz": "0c964522f25a27192c4facd7264e71e395d1c383b09276cf3617a02e77af3a74",
+	"gomobile-openal-soft-1.16.0.1.tar.gz":    "df8492a31030c4a940aa17602ea7ce56eb0759be9235f68fcce4c51150e49881",
 }
