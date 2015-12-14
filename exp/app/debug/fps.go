3abfce6c04852fa5715ddcbec33015035f41ce06
David Crawshaw <crawshaw@golang.org>
2015-12-14 09:11:54 -0800

    all: windows build tags
    For golang/go#9306
    
    Change-Id: Ia8778f5b371371d103c55c12606257ee7ca45fd4
    Reviewed-on: https://go-review.googlesource.com/17778
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- exp/app/debug/fps.go
+++ exp/app/debug/fps.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin linux
+// +build darwin linux windows
 
 // Package debug provides GL-based debugging tools for apps.
 package debug // import "golang.org/x/mobile/exp/app/debug"
