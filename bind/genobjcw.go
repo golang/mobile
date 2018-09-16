c7b62fd6a6d1b7092b54a6ae4ff6237e7af89a56
Tadas <tadovas@gmail.com>
2018-09-16 23:48:34 +0300

    Fix compilation error, fromslice second arg should be bool instead of int

--- bind/genobjcw.go
+++ bind/genobjcw.go
@@ -473,7 +473,7 @@ func (g *ObjcWrapper) genWrite(a *objc.Param) {
 	case objc.String:
 		g.Printf("_%s := encodeString(%s)\n", a.Name, a.Name)
 	case objc.Data:
-		g.Printf("_%s := fromSlice(%s, 0)\n", a.Name, a.Name)
+		g.Printf("_%s := fromSlice(%s, false)\n", a.Name, a.Name)
 	case objc.Bool:
 		g.Printf("_%s := %s(0)\n", a.Name, g.cgoType(a.Type))
 		g.Printf("if %s {\n", a.Name)
