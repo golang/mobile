golang/mobile

commit 11fe9ad4a06715288235cf14ebecb920d3cd6d7c

internal/importers: replace go/build usages with go/packages
This CL is a pure refactoring to replace go/build usage with
golang.org/x/tools/go/packages. This is a preparation for Go
modules.

Updates golang/go#27234

Change-Id: I3e6a30b962da1a64bc43a89a7f02c03d559f86d3
Reviewed-on: https://go-review.googlesource.com/c/mobile/+/189597
Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

cmd/gobind/main.go
@@ -11,7 +11,6 @@ import (
 	"go/ast"
 	"go/build"
 	"go/importer"
-	"go/parser"
 	"go/types"
 	"io/ioutil"
 	"log"
@@ -23,6 +22,7 @@ import (
 	"golang.org/x/mobile/internal/importers"
 	"golang.org/x/mobile/internal/importers/java"
 	"golang.org/x/mobile/internal/importers/objc"
+	"golang.org/x/tools/go/packages"
 )
 
 var (
@@ -53,17 +53,21 @@ func run() {
 	} else {
 		langs = []string{"go", "java", "objc"}
 	}
-	ctx := build.Default
-	if *tags != "" {
-		ctx.BuildTags = append(ctx.BuildTags, strings.Split(*tags, ",")...)
+
+	cfg := &packages.Config{
+		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles |
+			packages.NeedImports | packages.NeedDeps |
+			packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo,
+		BuildFlags: []string{"-tags", *tags},
+
+		// packages.Load invokes `go list` command with `GOOS=android`, but in most cases
+		// go-list cannot find the header files for Android. Suppress this error by
+		// disabling Cgo.
+		Env: append(os.Environ(), "CGO_ENABLED=0"),
 	}
-	var allPkg []*build.Package
-	for _, path := range flag.Args() {
-		pkg, err := ctx.Import(path, ".", build.ImportComment)
-		if err != nil {
-			log.Fatalf("package %q: %v", path, err)
-		}
-		allPkg = append(allPkg, pkg)
+	allPkg, err := packages.Load(cfg, flag.Args()...)
+	if err != nil {
+		log.Fatal(err)
 	}
 	jrefs, err := importers.AnalyzePackages(allPkg, "Java/")
 	if err != nil {
@@ -92,6 +96,12 @@ func run() {
 			log.Fatal(err)
 		}
 	}
+
+	ctx := build.Default
+	if *tags != "" {
+		ctx.BuildTags = append(ctx.BuildTags, strings.Split(*tags, ",")...)
+	}
+
 	// Determine GOPATH from go env GOPATH in case the default $HOME/go GOPATH
 	// is in effect.
 	if out, err := exec.Command("go", "env", "GOPATH").Output(); err != nil {
@@ -142,16 +152,12 @@ func run() {
 	imp := importer.For("source", nil)
 	for i, pkg := range allPkg {
 		var err error
-		typePkgs[i], err = imp.Import(pkg.ImportPath)
+		typePkgs[i], err = imp.Import(pkg.PkgPath)
 		if err != nil {
 			errorf("%v\n", err)
 			return
 		}
-		astPkgs[i], err = parse(pkg)
-		if err != nil {
-			errorf("%v\n", err)
-			return
-		}
+		astPkgs[i] = pkg.Syntax
 	}
 	for _, l := range langs {
 		for i, pkg := range typePkgs {
@@ -160,19 +166,6 @@ func run() {
 		// Generate the error package and support files
 		genPkg(l, nil, nil, typePkgs, classes, otypes)
 	}
-}
-
-func parse(pkg *build.Package) ([]*ast.File, error) {
-	fileNames := append(append([]string{}, pkg.GoFiles...), pkg.CgoFiles...)
-	var files []*ast.File
-	for _, name := range fileNames {
-		f, err := parser.ParseFile(fset, filepath.Join(pkg.Dir, name), nil, parser.ParseComments)
-		if err != nil {
-			return nil, err
-		}
-		files = append(files, f)
-	}
-	return files, nil
 }
 
 var exitStatus = 0

go.mod
@@ -5,4 +5,5 @@ go 1.11
 require (
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
+	golang.org/x/tools v0.0.0-20190808195139-e713427fea3f
 )

go.sum
@@ -3,7 +3,6 @@ golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACk
 golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529/go.mod h1:yigFU9vqHzYiE8UmvKecakEJjdnWj3jj499lnFckfCI=
 golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 h1:estk1glOnSVeJ9tdEZZc5mAMDZk5lNJNyJ6DvrBkTEU=
 golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56/go.mod h1:JhuoJpWY28nO4Vef9tZUw9qufEGTyX1+7lmHxV5q5G4=
-golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 h1:KYGJGHOQy8oSi1fDlSpcZF0+juKwk/hEMv5SiwHogR0=
 golang.org/x/image v0.0.0-20190227222117-0694c2d4d067/go.mod h1:kZ7UVZpmo3dzQBMxlp+ypCbDeSB+sBbTgSJuh5dn5js=
 golang.org/x/image v0.0.0-20190802002840-cff245a6509b h1:+qEpEAPhDZ1o0x3tHzZTQDArnOixOzGD9HUJfcg0mb4=
 golang.org/x/image v0.0.0-20190802002840-cff245a6509b/go.mod h1:FeLwcggjj3mMvU+oOTbSwawSJRM1uh48EjtB4UJZlP0=
@@ -11,8 +10,13 @@ golang.org/x/mobile v0.0.0-20190312151609-d3739f865fa6/go.mod h1:z+o9i4GpDbdi3rU
 golang.org/x/mod v0.1.0/go.mod h1:0QHyrYULN0/3qlju5TqG8bIK38QM8yzMo5ekMj3DlcY=
 golang.org/x/net v0.0.0-20190311183353-d8887717615a/go.mod h1:t9HGtf8HONx5eT2rtn7q6eTqICYqUVnKs3thJo3Qplg=
 golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3/go.mod h1:t9HGtf8HONx5eT2rtn7q6eTqICYqUVnKs3thJo3Qplg=
+golang.org/x/net v0.0.0-20190620200207-3b0461eec859/go.mod h1:z5CRVTTTmAJ677TzLLGU+0bjPO0LkuOLi4/5GtJWs/s=
+golang.org/x/sync v0.0.0-20190423024810-112230192c58/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
 golang.org/x/sys v0.0.0-20190412213103-97732733099d h1:+R4KGOnez64A81RvjARKc4UT5/tI9ujCIVX+P5KiHuI=
 golang.org/x/sys v0.0.0-20190412213103-97732733099d/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/tools v0.0.0-20190312151545-0bb0c0a6e846/go.mod h1:LCzVGOaR6xXOjkQ3onu1FJEFr0SW1gC7cKk1uF8kGRs=
+golang.org/x/tools v0.0.0-20190808195139-e713427fea3f h1:lSQQYboXWc71s9tnZRRBiMcc9Uc1BPWj3Bzvdk8UQ0Y=
+golang.org/x/tools v0.0.0-20190808195139-e713427fea3f/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
+golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

internal/importers/ast.go
@@ -28,14 +28,13 @@ package importers
 import (
 	"errors"
 	"go/ast"
-	"go/build"
-	"go/parser"
 	"go/token"
 	"path"
-	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
+
+	"golang.org/x/tools/go/packages"
 )
 
 // References is the result of analyzing a Go file or set of Go packages.
@@ -101,24 +100,19 @@ func AnalyzeFile(file *ast.File, pkgPrefix string) (*References, error) {
 
 // AnalyzePackages scans the provided packages for references to packages with the given
 // package prefix. The list of unique (package, identifier) pairs is returned
-func AnalyzePackages(pkgs []*build.Package, pkgPrefix string) (*References, error) {
+func AnalyzePackages(pkgs []*packages.Package, pkgPrefix string) (*References, error) {
 	visitor := newRefsSaver(pkgPrefix)
 	imp := visitor.importer()
 	fset := token.NewFileSet()
 	for _, pkg := range pkgs {
-		fileNames := append(append([]string{}, pkg.GoFiles...), pkg.CgoFiles...)
 		files := make(map[string]*ast.File)
-		for _, name := range fileNames {
-			f, err := parser.ParseFile(fset, filepath.Join(pkg.Dir, name), nil, 0)
-			if err != nil {
-				return nil, err
-			}
-			files[name] = f
+		for i, name := range pkg.CompiledGoFiles {
+			files[name] = pkg.Syntax[i]
 		}
 		// Ignore errors (from unknown packages)
 		astpkg, _ := ast.NewPackage(fset, files, imp, nil)
 		ast.Walk(visitor, astpkg)
-		visitor.findEmbeddingStructs(pkg.ImportPath, astpkg)
+		visitor.findEmbeddingStructs(pkg.PkgPath, astpkg)
 	}
 	return visitor.References, nil
 }

cmd/gobind/gobind_test.go
@@ -30,7 +30,7 @@ var tests = []struct {
 	{"Go-Testpkg", "go", "golang.org/x/mobile/bind/testdata/testpkg", "", false},
 	{"Java-Javapkg", "java", "golang.org/x/mobile/bind/testdata/testpkg/javapkg", "android", true},
 	{"Go-Javapkg", "go", "golang.org/x/mobile/bind/testdata/testpkg/javapkg", "android", true},
-	{"Go-Javapkg", "go,java,objc", "golang.org/x/mobile/bind/testdata/cgopkg", "android", false},
+	{"Go-Cgopkg", "go,java,objc", "golang.org/x/mobile/bind/testdata/cgopkg", "android", false},
 }
 
 var gobindBin string
@@ -109,10 +109,16 @@ type Struct struct{
 		t.Fatal(err)
 	}
 
+	gopath, err := exec.Command("go", "env", "GOPATH").Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	const comment = "This is a comment."
 	for _, lang := range []string{"java", "objc"} {
 		cmd := exec.Command(gobindBin, "-lang", lang, "doctest")
-		cmd.Env = append(os.Environ(), "GOROOT="+tmpdir)
+		// TODO(hajimehoshi): Enable this test with Go modules.
+		cmd.Env = append(os.Environ(), "GOPATH="+tmpdir+string(filepath.ListSeparator)+string(gopath), "GO111MODULE=off")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Errorf("gobind -lang %s failed: %v: %s", lang, err, out)

commit 77c542f388af466f149218c15e138807fc6c051a

cmd/gobind: fix build-tag, CGO and load trybot failures
* Correctly format build tags to pass into go/packages
* Removes CGO_ENABLED=0 from a packages.Load configuration
* Calls go/packages.Load twice to work around a build cache
* staleness issue

These bugs were introduced by CL 189597.

Updates golang/go#27234.
Updates golang/go#33687.

Change-Id: I3ae6737bf53bbecda0c7e25885b9c6aea5779332
Reviewed-on: https://go-review.googlesource.com/c/mobile/+/190479
Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>
Run-TryBot: Emmanuel Odeke <emm.odeke@gmail.com>

cmd/gobind/main.go
@@ -60,16 +60,16 @@ func run() {
 	}
 
 	cfg := &packages.Config{
-		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles |
+		Mode: packages.NeedName | packages.NeedFiles |
 			packages.NeedImports | packages.NeedDeps |
 			packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo,
-		BuildFlags: []string{"-tags", *tags},
-
-		// packages.Load invokes `go list` command with `GOOS=android`, but in most cases
-		// go-list cannot find the header files for Android. Suppress this error by
-		// disabling Cgo.
-		Env: append(os.Environ(), "CGO_ENABLED=0"),
+		BuildFlags: []string{"-tags", strings.Join(strings.Split(*tags, ","), " ")},
 	}
+
+	// Call Load twice to warm the cache. There is a known issue that the result of Load
+	// depends on build cache state. See golang/go#33687.
+	packages.Load(cfg, flag.Args()...)
+
 	allPkg, err := packages.Load(cfg, flag.Args()...)
 	if err != nil {
 		log.Fatal(err)

go.mod
@@ -5,5 +5,5 @@ go 1.11
 require (
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
-	golang.org/x/tools v0.0.0-20190808195139-e713427fea3f
+	golang.org/x/tools v0.0.0-20190816200558-6889da9d5479
 )

go.sum
@@ -17,6 +17,6 @@ golang.org/x/sys v0.0.0-20190412213103-97732733099d h1:+R4KGOnez64A81RvjARKc4UT5
 golang.org/x/sys v0.0.0-20190412213103-97732733099d/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/tools v0.0.0-20190312151545-0bb0c0a6e846/go.mod h1:LCzVGOaR6xXOjkQ3onu1FJEFr0SW1gC7cKk1uF8kGRs=
-golang.org/x/tools v0.0.0-20190808195139-e713427fea3f h1:lSQQYboXWc71s9tnZRRBiMcc9Uc1BPWj3Bzvdk8UQ0Y=
-golang.org/x/tools v0.0.0-20190808195139-e713427fea3f/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
+golang.org/x/tools v0.0.0-20190816200558-6889da9d5479 h1:lfN2PY/jymfnxkNHlbBF5DwPsUvhqUnrdgfK01iH2s0=
+golang.org/x/tools v0.0.0-20190816200558-6889da9d5479/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

internal/importers/ast.go
@@ -106,7 +106,7 @@ func AnalyzePackages(pkgs []*packages.Package, pkgPrefix string) (*References, e
 	fset := token.NewFileSet()
 	for _, pkg := range pkgs {
 		files := make(map[string]*ast.File)
-		for i, name := range pkg.CompiledGoFiles {
+		for i, name := range pkg.GoFiles {
 			files[name] = pkg.Syntax[i]
 		}
 		// Ignore errors (from unknown packages)

bind/java/seq_test.go
@@ -46,12 +46,11 @@ func testMain(m *testing.M) int {
 		if out, err := exec.Command(gocmd, "build", "-o", gobindBin, "golang.org/x/mobile/cmd/gobind").CombinedOutput(); err != nil {
 			log.Fatalf("gobind build failed: %v: %s", err, out)
 		}
-		PATH := os.Getenv("PATH")
-		if PATH != "" {
-			PATH += string(filepath.ListSeparator)
+		path := binDir
+		if oldPath := os.Getenv("PATH"); oldPath != "" {
+			path += string(filepath.ListSeparator) + oldPath
 		}
-		PATH += binDir
-		os.Setenv("PATH", PATH)
+		os.Setenv("PATH", path)
 	}
 	return m.Run()
 }

bind/objc/seq_test.go
@@ -9,9 +9,11 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -33,6 +35,43 @@ import (
 // All tests here require the Xcode command line tools.
 
 var destination = flag.String("device", "platform=iOS Simulator,name=iPhone 6s Plus", "Specify the -destination flag to xcodebuild")
+
+var gomobileBin string
+
+func TestMain(m *testing.M) {
+	os.Exit(testMain(m))
+}
+
+func testMain(m *testing.M) int {
+	binDir, err := ioutil.TempDir("", "bind-objc-test-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(binDir)
+
+	exe := ""
+	if runtime.GOOS == "windows" {
+		exe = ".exe"
+	}
+	if runtime.GOOS != "android" {
+		gocmd := filepath.Join(runtime.GOROOT(), "bin", "go")
+		gomobileBin = filepath.Join(binDir, "gomobile"+exe)
+		gobindBin := filepath.Join(binDir, "gobind"+exe)
+		if out, err := exec.Command(gocmd, "build", "-o", gomobileBin, "golang.org/x/mobile/cmd/gomobile").CombinedOutput(); err != nil {
+			log.Fatalf("gomobile build failed: %v: %s", err, out)
+		}
+		if out, err := exec.Command(gocmd, "build", "-o", gobindBin, "golang.org/x/mobile/cmd/gobind").CombinedOutput(); err != nil {
+			log.Fatalf("gobind build failed: %v: %s", err, out)
+		}
+		path := binDir
+		if oldPath := os.Getenv("PATH"); oldPath != "" {
+			path += string(filepath.ListSeparator) + oldPath
+		}
+		os.Setenv("PATH", path)
+	}
+
+	return m.Run()
+}
 
 // TestObjcSeqTest runs ObjC test SeqTest.m.
 func TestObjcSeqTest(t *testing.T) {
@@ -62,14 +101,11 @@ func TestObjcCustomPkg(t *testing.T) {
 }
 
 func runTest(t *testing.T, pkgNames []string, prefix, testfile, framework string, uitest, dumpOutput bool) {
+	if gomobileBin == "" {
+		t.Skipf("no gomobile on %s", runtime.GOOS)
+	}
 	if _, err := run("which xcodebuild"); err != nil {
 		t.Skip("command xcodebuild not found, skipping")
-	}
-	if _, err := run("which gomobile"); err != nil {
-		t.Log("go install gomobile")
-		if _, err := run("go install golang.org/x/mobile/cmd/gomobile"); err != nil {
-			t.Fatalf("gomobile install failed: %v", err)
-		}
 	}
 
 	tmpdir, err := ioutil.TempDir("", "bind-objc-seq-test-")
@@ -87,7 +123,7 @@ func runTest(t *testing.T, pkgNames []string, prefix, testfile, framework string
 		t.Fatalf("failed to copy %s: %v", testfile, err)
 	}
 
-	cmd := exec.Command("gomobile", "bind", "-target", "ios", "-tags", "aaa bbb")
+	cmd := exec.Command(gomobileBin, "bind", "-target", "ios", "-tags", "aaa bbb")
 	if prefix != "" {
 		cmd.Args = append(cmd.Args, "-prefix", prefix)
 	}