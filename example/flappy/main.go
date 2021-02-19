bdb1ca9a1e083af5929a8214e8a056d638ebbf2d
Russ Cox <rsc@golang.org>
2021-02-19 18:54:49 -0500

    all: go fmt ./...
    Make all our package sources use Go 1.17 gofmt format
    (adding //go:build lines).
    
    Not strictly necessary but will avoid spurious changes
    as files are edited.
    
    Part of //go:build change (#41184).
    See https://golang.org/design/draft-gobuild
    
    Change-Id: I30822eb504168b037ed3ec0f7759da1f41251f52
    Reviewed-on: https://go-review.googlesource.com/c/mobile/+/294374
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>
    Reviewed-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- example/flappy/main.go
+++ example/flappy/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || linux
 // +build darwin linux
 
 // Flappy Gopher is a simple one-button game that uses the
--- exp/gl/glutil/glimage_test.go
+++ exp/gl/glutil/glimage_test.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || (linux && !android)
 // +build darwin linux,!android
 
 // TODO(crawshaw): Run tests on other OSs when more contexts are supported.
