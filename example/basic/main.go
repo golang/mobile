efba26e336a8a0420b6614ad93b69575dab6807b
Russ Cox <rsc@golang.org>
2021-02-19 18:54:49 -0500

    all: go fmt ./...
    Make all our package sources use Go 1.17 gofmt format
    (adding //go:build lines).
    
    Not strictly necessary but will avoid spurious changes
    as files are edited.
    
    Part of //go:build change (#41184).
    See https://golang.org/design/draft-gobuild
    
    Change-Id: I30822eb504168b037ed3ec0f7759da1f41251f52
    Reviewed-on: https://go-review.googlesource.com/c/mobile/+/294374
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>
    Reviewed-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- example/basic/main.go
+++ example/basic/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || linux || windows
 // +build darwin linux windows
 
 // An app that draws a green triangle on a red background.
--- exp/audio/al/al.go
+++ exp/audio/al/al.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || linux || windows
 // +build darwin linux windows
 
 // Package al provides OpenAL Soft bindings for Go.
