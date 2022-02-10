032b21455fcfcfeeeea6a22c6852c8486615ac83
Zaiyang Li <zaiyangli777@gmail.com>
2022-02-10 20:58:22 +0000

    cmd/gomobile: add missing latest tag to gobind
    Go install requires a version to be provided after the package name.
    gomobile init command is updated to reflect this new requirement
    
    Fixes golang/go#50994
    
    Change-Id: I014bd6815d9620415ceb43dd7e0aea3afac60556
    GitHub-Last-Rev: 3a07fa669175c34b5948de4abe74ab9092c83934
    GitHub-Pull-Request: golang/mobile#75
    Reviewed-on: https://go-review.googlesource.com/c/mobile/+/384414
    Trust: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- cmd/gomobile/init.go
+++ cmd/gomobile/init.go
@@ -78,7 +78,7 @@ func runInit(cmd *command) error {
 	}()
 
 	// Make sure gobind is up to date.
-	if err := goInstall([]string{"golang.org/x/mobile/cmd/gobind"}, nil); err != nil {
+	if err := goInstall([]string{"golang.org/x/mobile/cmd/gobind@latest"}, nil); err != nil {
 		return err
 	}
 
